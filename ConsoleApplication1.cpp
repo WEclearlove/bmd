572871af3604d6109e2f2c155743b031f9688465
WEclearlove <48906226+WEclearlove@users.noreply.github.com>
2019-05-09 09:59:55 +0800

    caomei
    fzzf

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+class CTestCopy
+{
+	int x;
+	int y;
+	public;
+	CTestCopy;
+	{
+
+	};
+	CTestCopy(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+	void setXY(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+	void disPlay(
+	{
+		cout << "x=" << x << "y=" << y << endl;
+	}
+ };
+
+int main()
+{
+	CTestCopy ocpy(500, 800)
+	ocpy.dispaly();
+	ocpy.srtXY(100, 230);
+	ocpy.disPlay();
+    return 0;
+}
+
